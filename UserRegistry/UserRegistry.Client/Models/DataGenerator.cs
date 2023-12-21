86c515253a226bffec7938993ca20a2ed5ff3c24
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-21 15:54:59 +0400

    Refactors DataGenerator class
    Refactored data generator class and
    replaced mouthfull rules declaration
    with bulk rules syntax.

--- UserRegistry/UserRegistry.Client/Models/DataGenerator.cs
+++ UserRegistry/UserRegistry.Client/Models/DataGenerator.cs
@@ -5,25 +5,24 @@ namespace UserRegistry.Client.Models;
 public class DataGenerator(string locale)
 {
     private readonly Faker<PersonModel> personModelFake = new Faker<PersonModel>(locale)
-            .RuleFor(u => u.Number, f => f.IndexGlobal + 1)
-            .RuleFor(u => u.Id, f => f.Random.Int(1, 10000))
-            .RuleFor(u => u.Gender, f => f.Person.Gender.ToString())
-            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
-            .RuleFor(u => u.LastName, f => f.Name.LastName())
-            .RuleFor(u => u.MiddleName, f =>
-                f.Name.FirstName(f.Person.Gender).OrNull(f, .2f))
-            .RuleFor(u => u.Email, (f, u) =>
-                f.Internet.Email(u.FirstName, u.LastName))
-            .RuleFor(u => u.Phone, f => f.Phone.PhoneNumber())
-            .RuleFor(u => u.StreetAddress, f => f.Address.FullAddress())
-            .RuleFor(u => u.City, f => f.Address.City())
-            .RuleFor(u => u.State, f => f.Address.StateAbbr())
-            .RuleFor(u => u.ZipCode, f => f.Address.ZipCode())
-            .RuleFor(u => u.Rating, f => f.PickRandom<CreditRating>())
-            .RuleFor(u => u.Name, f =>
-            $"{f.Name.FirstName()} " +
-            $"{f.Name.FirstName(f.Person.Gender).OrNull(f, .2f)} " +
-            $"{f.Name.LastName()}");
+        .StrictMode(false)
+        .Rules((f, u) =>
+        {
+            u.Number = f.IndexGlobal + 1;
+            u.Id = f.Random.Int(1, 10000);
+            u.Gender = f.Person.Gender.ToString();
+            u.FirstName = f.Name.FirstName();
+            u.LastName = f.Name.LastName();
+            u.MiddleName = f.Name.FirstName(f.Person.Gender).OrNull(f, .2f);
+            u.Email = f.Internet.Email(u.FirstName, u.LastName);
+            u.Phone = f.Phone.PhoneNumber();
+            u.StreetAddress = f.Address.FullAddress();
+            u.City = f.Address.City();
+            u.State = f.Address.StateAbbr();
+            u.ZipCode = f.Address.ZipCode();
+            u.Rating = f.PickRandom<CreditRating>();
+            u.Name = $"{u.FirstName} {u.MiddleName} {u.LastName}";
+        });
 
     public PersonModel GeneratePerson(int seed)
     {
